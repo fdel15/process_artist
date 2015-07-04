242aae79ccee46ac9b7a49aa1a8db71f88febf86
Frank DelPidio <frankdaedalus@gmail.com>
2015-07-04 16:40:33 -0400

    use keys pressed to change background color

--- process_artist.rb
+++ process_artist.rb
@@ -10,6 +10,19 @@ class ProcessArtist < Processing::App
     # Do Stuff
   end
 
+  def run_command(command)
+    if command[0] == 'b'
+      set_background_color(command[1..-1])
+    else
+      warn "Sorry I do not recognize that command. Please try again"
+    end
+  end
+
+  def set_background_color(string)
+    colors = string.split(",").map{|num| num.to_i}
+    background(colors[0], colors[1], colors[2])
+  end
+
   def key_pressed
     warn "A key was pressed! #{key.inspect}"
     if @queue.nil?
@@ -20,6 +33,7 @@ class ProcessArtist < Processing::App
       @queue = @queue + key
     else
       warn "Time to run the command : #{@queue}"
+      run_command(@queue)
       @queue = ""
     end
   end
